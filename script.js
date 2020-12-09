26f934e793e17a80c0cf85857de5149de10b4dd0
RachelNS <rachelnelsonschille@gmail.com>
2020-12-08 16:49:34 -0800

    added pseudocode and prompts to js file

--- script.js
+++ script.js
@@ -8,7 +8,38 @@ function writePassword() {
 
   passwordText.value = password;
 
+  
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// TODO: Define lowercase letters
+// TODO: Define uppercase letters
+// TODO: Define random whole numbers 1-10
+// TODO: Define special characters
+
+
+// Take user input
+  //Ask how many characters in password (must be between 8 and 128)
+  var numCharacters = prompt("How many characters should your password have?")
+  // TODO: If the number is too low, tell them it's too low. If the number is too high, tell them it's too high.
+
+  //Ask if they want lowercase letters
+  var lowerCase = confirm("Would you like lowercase letters in your password?")
+  //TODO: If yes, retrieve lowercase letters
+
+  // Ask if they want uppercase letters
+  var upperCase = confirm("Would you like uppercase letters in your password?")
+  // TODO: If yes, retrieve uppercase letters
+
+  //Ask if they want numbers
+  var confirmNum = confirm("Would you like numbers in your password?")
+  // TODO: If yes, retrieve numbers
+
+  // Ask if they want special characters
+  var specialChar = confirm("Would you like special characters in your password?")
+  // TODO: If yes, retrieve special characters
+
+
